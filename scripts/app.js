909c4f6dcacef5781e2808a190d1daeac21a5c44
Jeff Johnson <uhohchongo@hotmail.com>
2015-12-07 20:04:45 -0800

    add directives for panels

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,23 @@
 (function(){
 	var app = angular.module('store', []);
 	
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				this.tab = 1;
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+				};
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: 'panels'
+		};
+	});
+	
 	app.directive('productTitle', function(){
 		return {
 			restrict: 'E',
@@ -13,16 +30,6 @@
 		this.addReview = function(product){
 			product.reviews.push(this.review);
 			this.review = {};
-		};
-	});
-	
-	app.controller('PanelController', function(){
-		this.tab = 1;
-		this.selectTab = function(setTab){
-			this.tab = setTab;
-		};
-		this.isSelected = function(checkTab){
-			return this.tab === checkTab;
 		};
 	});
 	
